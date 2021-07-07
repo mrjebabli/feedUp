10094cdd5feca9037c88d62580a8aeef0657d4d3
mohamed riadh jebabli <26mohamed.jebabli@gmail.com>
2021-07-07 22:04:28 +0100

    adding some

--- feedUp_backEnd/src/Controller/UtilisateurController.php
+++ feedUp_backEnd/src/Controller/UtilisateurController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Utilisateur;
 use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
     /**
@@ -34,7 +38,7 @@ class UtilisateurController extends AbstractController
         //$encoders = array(new JsonEncoder());
         //$serializer = new Serializer([new ObjectNormalizer()], $encoders);
         //Using the annotation groups to serialize uniquement les attributs qu'on veut 
-        $data = $serializer->serialize($list, 'json',['groups'=>'event:read']);
+        $data = $serializer->serialize($list, 'json');
         $response = new Response($data, 200);
         //content type
         $response->headers->set('Content-Type', 'application/json');
@@ -58,12 +62,16 @@ class UtilisateurController extends AbstractController
        $us->remove($user);
        $us->flush();
        $response = new Response('', Response::HTTP_OK);
-       //Allow all websites
+       
        $response->headers->set('Access-Control-Allow-Origin', '*');
-       // You can set the allowed methods too, if you want
        $response->headers->set('Access-Control-Allow-Methods', 'DELETE');
        return $response;
    }
+
+    
+
+    
+
     
 
 }
